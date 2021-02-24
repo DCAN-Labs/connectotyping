730919c80f7b0b64268ada7da18bdc55bd0f2e5d
paul-reiners <paul.reiners@gmail.com>
2021-02-24 09:27:43 -0600

    Added data point.

--- experiments/estimating_time_versus_rois/show_roi_vs_time_plot.m
+++ experiments/estimating_time_versus_rois/show_roi_vs_time_plot.m
@@ -1,8 +1,13 @@
 M = readmatrix('rois_versus_time.csv')
 col1 = M(:, 1);
 col2 = M(:, 2);
+col3 = M(:, 3);
 figure
-plot(col1, col2)
+plot(col1, col2,'DisplayName','Without parfor')
+hold on 
+plot(col1, col3,'DisplayName','With parfor in model_tsvd')
+hold off
 title('ROI count versus computation time')
 xlabel('Number of ROIs') 
 ylabel('Computation time (in seconds)') 
+legend('Location', 'best')
